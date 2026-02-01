14af7685e2d2a029cbe54000c44cd16cb83eb83a
Obal D Almeida <94952806+oba69@users.noreply.github.com>
2026-02-01 09:53:48 +0530

    added seperate health check function

---
+++ api/healthz.js
@@ -0,0 +1,11 @@
+import { healthCheck } from "../db";
+
+export default async function handler(req, res) {
+    try {
+        const ok = await healthCheck();
+        const statusCode = ok ? 200 : 500;
+        res.status(statusCode).json({ ok });
+    } catch (err) {
+        res.status(500).json({ ok: false, error: "Health check failed" });
+    }
+}
